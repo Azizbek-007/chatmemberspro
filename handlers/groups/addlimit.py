a81e4c51926194e46a466d29b746f62706e2f3f8
Azizbek-h <texnoposnode@gmail.com>
2023-02-13 22:11:49 +0500

    text add

--- handlers/groups/addlimit.py
+++ handlers/groups/addlimit.py
@@ -6,13 +6,11 @@ from filters import IsAdmin
 @dp.message_handler(IsAdmin(), commands=['add'], is_chat_admin=True, chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 async def bot_addlimit(msg: types.Message):
     add_count = msg.text.split(' ')
-    if len(add_count) == 2:
+    if len(add_count) > 0:
         try: 
             add_count_number = int(add_count[1])
-            group_data= await dp.bot.get_chat(msg.chat.id)
-            DBS.set_group_premissions(DBS, group_id=msg.chat.id, premissions=group_data.permissions)
             DBS.add_member_count(DBS, msg.chat.id, add_count_number)
             await msg.answer("Qosildi")
         except TypeError:
-            await msg.answer("/addlimit 10") 
-    else: await msg.reply("/addlimit 10")
+            await msg.answer("Дурыс жазың!\n\nМысалы: /addlist 10\n❗️Еслетпе: 10 санының орнына қәлеген санды жазыўыңызға болады.") 
+    else: await msg.answer("Дурыс жазың!\n\nМысалы: /addlist 10\n")
--- handlers/groups/clearall.py
+++ handlers/groups/clearall.py
@@ -3,10 +3,6 @@ from loader import dp
 from utils.db_api import DBS
 from filters import IsAdmin
 
-@dp.message_handler(IsAdmin(), commands="clear",  is_chat_admin=True, chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
-async def bot_clear(msg: types.Message):
-    await msg.answer("Qa'te buyriq berdin'iz!")
-
 @dp.message_handler(IsAdmin(), commands="clear", is_reply=True, is_chat_admin=True, chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 async def bot_clear(msg: types.Message):
     reply_user_id = msg.reply_to_message.from_id
@@ -16,4 +12,8 @@ async def bot_clear(msg: types.Message):
 @dp.message_handler(IsAdmin(), commands="clearall", is_chat_admin=True, chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 async def bot_clear_all(msg: types.Message):
     DBS.clear_all_group(DBS, msg.chat.id)
-    await msg.answer("Gruppa maglumatlari tazalandi")
+    await msg.answer(f"<a href='tg://user?id={msg.chat.id}'>{msg.chat.title}</a> группасындағы мағлыўматлар тазаланды!")
+
+@dp.message_handler(IsAdmin(), commands="clear", is_chat_admin=True, chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
+async def bot_clear(msg: types.Message):
+    await msg.answer("Дурыс жазың!\n\nМысалы: /clear (ID яки reply)")
